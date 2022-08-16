0cbfe5769dab83e8fc45e3c2251573443653a4d5
Alin Cristian <alin.cristian977@gmail.com>
2022-08-16 12:24:49 +0300

    converting existing EF data annotations into ModelBuilder settings via EF fluent api

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -14,6 +14,29 @@ namespace WorldCitiesAPI.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<City>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<City>()
+                .Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>()
+                .Property(x => x.Lat).HasColumnType("decimal(7,4)");
+            modelBuilder.Entity<City>()
+                .Property(x => x.Lon).HasColumnType("decimal(7,4)");
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Country>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Country>()
+                .Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>()
+                .HasOne(x => x.Country)
+                .WithMany(y => y.Cities)
+                .HasForeignKey(x => x.CountryId);
+        }
+
         /// <summary>
         /// The Cities table
         /// </summary>
